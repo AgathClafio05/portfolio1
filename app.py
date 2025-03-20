9accd219856e96f065ee43a7d7ec90ba681c47a5
Agath Clafio <114557109+AgathClafio05@users.noreply.github.com>
2025-03-20 20:06:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ with st.container():
     contact_form = """
    <form action="https://formsubmit.co/agathclafio55@gmail.com" method="POST">
     <input type="hidden" name="_captcha" value="false">
-    <input type="text" name="name" placeholder="Your name" required style="color: white;>
+    <input type="text" name="name" placeholder="Your name" required>
     <input type="email" name="email" placeholder="Your email" required>
     <textarea name="message" placeholder="Your message here" required></textarea>
     <button type="submit">Send</button>
